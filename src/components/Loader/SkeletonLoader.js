af7137cac9fd1e6e5d7982a23812c221808c71b5
serkancagman <serkancagmann@gmail.com>
2022-02-17 15:28:25 +0300

    skeleton options

--- src/components/Loader/SkeletonLoader.js
+++ src/components/Loader/SkeletonLoader.js
@@ -31,22 +31,22 @@ export const ProductSkeleton = () => {
         width="200"
         height="200"
       />
-      <rect x="30" y="250" rx="8" ry="10" width="200" height="18" />
+      <rect x="30" y="250" rx="8" ry="10" width="200" height="20" />
       <rect x="30" y="275" rx="8" ry="10" width="120" height="20" />
       <rect x="250" y="20" rx="8" ry="8" width="200" height="200" />
-      <rect x="250" y="250" rx="8" ry="10" width="200" height="18" />
+      <rect x="250" y="250" rx="8" ry="10" width="200" height="20" />
       <rect x="250" y="275" rx="8" ry="10" width="120" height="20" />
       <rect x="470" y="20" rx="8" ry="8" width="200" height="200" />
-      <rect x="470" y="250" rx="8" ry="10" width="200" height="18" />
+      <rect x="470" y="250" rx="8" ry="10" width="200" height="20" />
       <rect x="470" y="275" rx="8" ry="10" width="120" height="20" />
       <rect x="690" y="20" rx="8" ry="8" width="200" height="200" />
-      <rect x="690" y="250" rx="8" ry="10" width="200" height="18" />
+      <rect x="690" y="250" rx="8" ry="10" width="200" height="20" />
       <rect x="690" y="275" rx="8" ry="10" width="120" height="20" />
       <rect x="910" y="20" rx="8" ry="8" width="200" height="200" />
-      <rect x="910" y="250" rx="8" ry="10" width="200" height="18" />
+      <rect x="910" y="250" rx="8" ry="10" width="200" height="20" />
       <rect x="910" y="275" rx="8" ry="10" width="120" height="20" />
       <rect x="1130" y="20" rx="8" ry="8" width="200" height="200" />
-      <rect x="1130" y="250" rx="8" ry="10" width="200" height="18" />
+      <rect x="1130" y="250" rx="8" ry="10" width="200" height="20" />
       <rect x="1130" y="275" rx="8" ry="10" width="120" height="20" />
     </ContentLoader>
   );
